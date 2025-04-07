d19f7834dda5a931d999c58953d14e893ffb315f
Person552 <qaustinbarnes@gmail.com>
2025-04-07 15:45:02 -0700

    test commit

--- src/main/java/net/person552/cursedrelics/CursedRelics.java
+++ src/main/java/net/person552/cursedrelics/CursedRelics.java
@@ -2,6 +2,7 @@ package net.person552.cursedrelics;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.person552.cursedrelics.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,7 +19,6 @@ public class CursedRelics implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/person552/cursedrelics/item/ModItems.java
@@ -0,0 +1,26 @@
+package net.person552.cursedrelics.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.person552.cursedrelics.CursedRelics;
+
+public class ModItems {
+
+    public static final Item CURSED_FRAGMENT = registerItem("cursed_fragment", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(CursedRelics.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        CursedRelics.LOGGER.info("Registering Mod Items for " + CursedRelics.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(CURSED_FRAGMENT);
+        });
+    }
+}
