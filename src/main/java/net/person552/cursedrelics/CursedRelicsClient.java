f18e9b47566553aa4bb2dc190d96dca5bad7c249
Person552 <qaustinbarnes@gmail.com>
2025-04-07 14:47:57 -0700

    Inital Commit

---
+++ src/main/java/net/person552/cursedrelics/CursedRelicsClient.java
@@ -0,0 +1,10 @@
+package net.person552.cursedrelics;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CursedRelicsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/person552/cursedrelics/CursedRelicsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.person552.cursedrelics;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CursedRelicsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
