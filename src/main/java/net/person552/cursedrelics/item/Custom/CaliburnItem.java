a4d8ac5ee4b739659ed141db741b03bafca9ca39
Person552 <qaustinbarnes@gmail.com>
2025-04-07 22:47:59 -0700

    give caliburn tooltip

--- src/main/java/net/person552/cursedrelics/item/Custom/CaliburnItem.java
+++ src/main/java/net/person552/cursedrelics/item/Custom/CaliburnItem.java
@@ -1,9 +1,14 @@
 package net.person552.cursedrelics.item.Custom;
 
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.SwordItem;
 import net.minecraft.item.ToolMaterial;
+import net.minecraft.item.tooltip.TooltipType;
+import net.minecraft.text.Text;
+
+import java.util.List;
 
 public class CaliburnItem extends SwordItem {
     public CaliburnItem(ToolMaterial toolMaterial, Settings settings) {
@@ -16,5 +21,16 @@ public class CaliburnItem extends SwordItem {
             target.setFireTicks(180);
         }
         return super.postHit(stack, target, attacker);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType type) {
+        if (Screen.hasShiftDown()) {
+            tooltip.add(Text.translatable("tooltip.cursedrelics.caliburn"));
+        } else {
+            tooltip.add(Text.translatable("tooltip.cursedrelics.shiftinfo"));
+        }
+
+        super.appendTooltip(stack, context, tooltip, type);
     }
 }
