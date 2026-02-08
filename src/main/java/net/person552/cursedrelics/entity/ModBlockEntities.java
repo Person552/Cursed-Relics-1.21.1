f88b9d23454e2d070cbc7552f42ddc074ba23798
Person552 <qaustinbarnes@gmail.com>
2026-02-08 13:21:16 -0800

    Trial Spawner + Vault Nonsense

--- src/main/java/net/person552/cursedrelics/entity/ModBlockEntities.java
+++ src/main/java/net/person552/cursedrelics/entity/ModBlockEntities.java
@@ -7,15 +7,16 @@ import net.minecraft.util.Identifier;
 import net.person552.cursedrelics.CursedRelics;
 import net.person552.cursedrelics.block.ModBlocks;
 import net.person552.cursedrelics.entity.custom.CursedTrialSpawnerBlockEntity;
+import net.person552.cursedrelics.entity.custom.CursedVaultBlockEntity;
 
 public class ModBlockEntities {
     public static final BlockEntityType<CursedTrialSpawnerBlockEntity> CURSED_TRIAL_SPAWNER_BE =
             Registry.register(Registries.BLOCK_ENTITY_TYPE, Identifier.of(CursedRelics.MOD_ID, "cursed_trial_spawner_be"),
                     BlockEntityType.Builder.create(CursedTrialSpawnerBlockEntity::new, ModBlocks.CURSED_TRIAL_SPAWNER).build(null));
 
-    public static final BlockEntityType<CursedTrialSpawnerBlockEntity> CURSED_VAULT_BE =
+    public static final BlockEntityType<CursedVaultBlockEntity> CURSED_VAULT_BE =
             Registry.register(Registries.BLOCK_ENTITY_TYPE, Identifier.of(CursedRelics.MOD_ID, "cursed_vault_be"),
-                    BlockEntityType.Builder.create(CursedTrialSpawnerBlockEntity::new, ModBlocks.CURSED_VAULT).build(null));
+                    BlockEntityType.Builder.create(CursedVaultBlockEntity::new, ModBlocks.CURSED_VAULT).build(null));
 
     public static void registerBlockEntities() {
         CursedRelics.LOGGER.info("Registering Mod Block Entities for " + CursedRelics.MOD_ID);
--- src/main/java/net/person552/cursedrelics/entity/custom/CursedVaultBlockEntity.java
+++ src/main/java/net/person552/cursedrelics/entity/custom/CursedVaultBlockEntity.java
@@ -1,14 +1,37 @@
 package net.person552.cursedrelics.entity.custom;
 
 import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.block.entity.VaultBlockEntity;
-import net.minecraft.block.vault.VaultClientData;
+import net.minecraft.block.spawner.EntityDetector;
 import net.minecraft.block.vault.VaultConfig;
-import net.minecraft.block.vault.VaultServerData;
-import net.minecraft.block.vault.VaultSharedData;
+import net.minecraft.item.ItemStack;
+import net.minecraft.loot.LootTables;
 import net.minecraft.util.math.BlockPos;
-import org.jetbrains.annotations.Nullable;
+import net.person552.cursedrelics.entity.ModBlockEntities;
+import net.person552.cursedrelics.item.ModItems;
 
-public class CursedVaultBlockEntity {
+import java.util.Optional;
 
+public class CursedVaultBlockEntity extends VaultBlockEntity {
+    public CursedVaultBlockEntity(BlockPos pos, BlockState state) {
+        super(pos, state);
+        this.setConfig(new VaultConfig(LootTables.TRIAL_CHAMBERS_REWARD_CHEST,
+                (double)4.0F,
+                (double)4.5F,
+                new ItemStack(ModItems.CURSED_KEY),
+                Optional.empty(),
+                EntityDetector.NON_SPECTATOR_PLAYERS,
+                EntityDetector.Selector.IN_WORLD));
+    }
+
+    @Override
+    public BlockEntityType<?> getType() {
+        return ModBlockEntities.CURSED_VAULT_BE;
+    }
+
+    @Override
+    public boolean supports(BlockState state) {
+        return ModBlockEntities.CURSED_VAULT_BE.supports(state);
+    }
 }
